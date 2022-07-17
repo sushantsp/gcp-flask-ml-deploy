e5b850ffc4156f084e3b2bb6c7b3e912fede1fa0
Sushant <32874962+sushantsp@users.noreply.github.com>
2022-07-17 17:40:53 +0530

    testing cts delivery

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CD'
+    return 'Hello World, THis is checking for CD !!'
 
 @app.route('/echo/<name>')
 def echo(name):
